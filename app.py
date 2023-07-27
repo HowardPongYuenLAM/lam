157f6aa8a3b3725ba8c963ec268c7378240a5f8d
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 19:00:00 +0800

    July27-v1

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ def main():
     st.set_page_config(page_title="Ask pdf")
     st.header("test key")
 #    response="lampongyuen"
-    response=working_key
+    response=GCP_PROJECT_ID
     st.write(response)
     
 if __name__ == '__main__':
     load_dotenv(verbose=True)
+    GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+    SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+    STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
     working_key = os.environ.get("mykey2")
+    from dotenv import dotenv_values
+    print(dotenv_values)
     main()
