3f7550cf85df993174d5c9b5d97ab80f4b99ba00
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 17:03:52 +0800

    July27-v1

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
-import mytoken as *
+import mytoken
 
 def main():
-    mytoken
-#    os.environ['test']='pqrst'
+    os.environ['test']='pqrst'
     st.set_page_config(page_title="Ask")
     st.header("Ask your PDF testing")
     response=os.environ['test']
     st.write(response)
+    
 if __name__ == '__main__':
+    mytoken
     main()
