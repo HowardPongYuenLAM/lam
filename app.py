0ace596eeccf880a1273c9c83bb1c4656076ef89
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 17:24:24 +0800

    July27-v1

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from mytoken import *
 
 def main():
     st.set_page_config(page_title="Ask")
-    st.header("Ask your PDF testing")
-    response=os.environ['test']
-    st.write(response)
-    response=os.environ['my_API_key']
+    st.header("test key")
+#    response=os.environ['test']
+    response=mykey
     st.write(response)
     
 if __name__ == '__main__':
