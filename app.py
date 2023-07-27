55f042d07dba1bb51ca01810d5767b233233d706
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 19:08:47 +0800

    July27-v1

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from pathlib import Path
 def main():
     st.set_page_config(page_title="Ask pdf")
     st.header("test key3")
-    response=SERVICE_ACCOUNT_FILE
+    response=HUGGINGFACEHUB_API_TOKEN
     st.write(response)
     
 if __name__ == '__main__':
 #    dotenv_path = Path('path/to/.env')
 #    load_dotenv(dotenv_path=dotenv_path,verbose=True)
     load_dotenv(verbose=True)
+    HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
     GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
     SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
     STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
