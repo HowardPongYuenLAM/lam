3d8dc3a62ff615e59142f4f7063e08d785e61e80
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 16:48:24 +0800

    July27-v1

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 import os
 
 def main():
-    os.environ['test']='pqr'
+    os.environ['test']='pqrst'
     st.set_page_config(page_title="Ask your PDF there ")
     st.header("Ask your PDF there")
     response=os.environ['test']
     st.write(response)
-   
-
 if __name__ == '__main__':
     main()
