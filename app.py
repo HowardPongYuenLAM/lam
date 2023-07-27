e0ed133cd3fb1585600eea76ddcc0c567a760068
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 18:35:47 +0800

    July27-v1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import os
 from mytoken import *
+from dotenv import load_dotenv
 
 def main():
     st.set_page_config(page_title="Ask pdf")
     st.header("test key")
-    response=os.environ('mykey2')
+#    response=os.environ('mykey2')
+    response="api"
     st.write(response)
     
 if __name__ == '__main__':
