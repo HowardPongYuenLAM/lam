fbf92498032774c0dd48ed29f5c7c2b85fd5feaf
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 14:14:01 +0800

    July27

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import os
-
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 💬")
     response=os.environ['HUGGINGFACEHUB_API_TOKEN']
