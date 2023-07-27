9e1548c90e5690ff7679824e4c95c485f8274cca
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 18:42:14 +0800

    July27-v1

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 def main():
     st.set_page_config(page_title="Ask pdf")
     st.header("test key")
-    response=os.environ('mykey2')
     response="api"
     st.write(response)
     
 if __name__ == '__main__':
     load_dotenv(verbose=True)
+    working_key = os.environ(['mykey2'])
     main()
