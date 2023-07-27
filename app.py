dd442749de9477e2f62f8837797ebabdbfa47d33
Howard Lam <pongyuenlam@gmail.com>
2023-07-27 16:55:29 +0800

    July27-v1

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 
 def main():
 #    os.environ['test']='pqrst'
-    st.set_page_config(page_title="Ask your PDF there ")
-    st.header("Ask your PDF there")
+    st.set_page_config(page_title="Ask")
+    st.header("Ask your PDF testing")
     response=os.environ['test']
     st.write(response)
 if __name__ == '__main__':
