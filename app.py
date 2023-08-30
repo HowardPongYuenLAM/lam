babc2c9df0cf58c51f8a26a86b6a2fd205bfc242
HowardPongYuenLAM <121382449+HowardPongYuenLAM@users.noreply.github.com>
2023-08-30 15:38:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import mysql.connector
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return mysql.connector.connect()
+    return mysql.connector.connect(**st.secrets.db_crentials)
 #    return mysql.connector.connect(**st.secrets["pets"])
 
 conn = init_connection()
